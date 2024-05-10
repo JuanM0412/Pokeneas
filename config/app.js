5cc564903f060dcfcc326dad2b73cbbacdf3ecf4
Miguel Hoyos <mahoyosv@eafit.edu.co>
2024-05-09 19:53:45 -0500

    Port changed

--- config/app.js
+++ config/app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
 
 dotenv.config();
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 app.set('port', port)
 app.set('views', path.join(__dirname, '../app/views'))
 app.set('view engine', 'ejs')
 
-module.exports = app;
+module.exports = app
